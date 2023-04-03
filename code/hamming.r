2934c9499b204971a6bb2f15113adf63ed24a152
Julian Holfeld <julian@uni-kassel.de>
2023-04-03 15:37:25 +0200

    refactor: use function for hamming_distance

--- code/hamming.r
+++ code/hamming.r
@@ -8,34 +8,41 @@ hamming_distance <- function(a, b) {
   return(sum(a != b))
 }
 
-person_matrices <- lapply(seq_len(nrow(persons)), function(i) {
-  person <- persons[i, 2:ncol(persons)]
-  person_articles <- articles[match(person, articles$Nr),]
-  person_row <- person_articles[, -1]
-  names(person_row) <- colnames(articles)[-1]
+plot_hamming_distance <- function(article_file_name, person_file_name) {
+  articles <- read.csv(paste0("./code/", article_file_name, ".csv"), sep = ";", header = TRUE, check.names = FALSE, encoding = "UTF-8")
+  persons <- read.csv(paste0("./code/", person_file_name, ".csv"), sep = ";", header = TRUE, check.names = FALSE, encoding = "UTF-8")
 
-  return(person_row)
-})
+  person_matrices <- lapply(seq_len(nrow(persons)), function(i) {
+    person <- persons[i, 2:ncol(persons)]
+    person_articles <- articles[match(person, articles$Nr),]
+    person_row <- person_articles[, -1]
+    names(person_row) <- colnames(articles)[-1]
 
-person_distances <- lapply(person_matrices, function(person_row) {
-  distance <- sum(sapply(1:(nrow(person_row) - 1), function(j) {
-    hamming_distance(person_row[j,], person_row[j + 1,])
-  }))
+    return(person_row)
+  })
 
-  return(distance)
-})
+  person_distances <- lapply(person_matrices, function(person_row) {
+    distance <- sum(sapply(1:(nrow(person_row) - 1), function(j) {
+      hamming_distance(person_row[j,], person_row[j + 1,])
+    }))
 
-person_matrices_df <- do.call(rbind.data.frame, person_matrices)
-person_distances_df <- data.frame(
-  Person = seq_len(length(person_distances)),
-  Distance = unlist(person_distances)
-)
+    return(distance)
+  })
 
-ggplot(person_distances_df, aes(x = Person, y = Distance, fill = as.factor(Person))) +
-  geom_bar(stat = "identity") +
-  theme_minimal() +
-  labs(x = "Person", y = "Gesamtdistanz", fill = "Person", title = "Hamming-Distanzen zwischen aufeinanderfolgenden Artikeln") +
-  theme(legend.position = "none") +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  scale_x_continuous(breaks = seq(1, nrow(persons), 1)) +
-  scale_y_continuous(breaks = seq(1, 10, 1))
+  person_distances_df <- data.frame(
+    Person = seq_len(length(person_distances)),
+    Distance = unlist(person_distances)
+  )
+
+  ggplot(person_distances_df, aes(x = Person, y = Distance, fill = as.factor(Person))) +
+    geom_bar(stat = "identity") +
+    theme_minimal() +
+    labs(x = "Person", y = "Gesamtdistanz", fill = "Person", title = "Hamming-Distanzen zwischen aufeinanderfolgenden Artikeln") +
+    theme(legend.position = "none") +
+    theme(plot.title = element_text(hjust = 0.5)) +
+    scale_x_continuous(breaks = seq(1, nrow(persons), 1)) +
+    scale_y_continuous(breaks = seq(1, 12, 1))
+}
+
+plot_hamming_distance("list-articles", "list-persons")
+# plot_hamming_distance("fca-articles", "fca-persons")
