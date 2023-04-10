32c1c521b67c6a57743413000f6213d7fb00928c
Julian Holfeld <julian@uni-kassel.de>
2023-04-10 19:44:53 +0200

    refactor: use age-boxplot instead of hist

--- code/age-boxplot.r
+++ code/age-boxplot.r
@@ -4,8 +4,16 @@ p_load(pacman, ggplot2)
 data <- read.csv("./code/age.csv", sep = ";", header = TRUE, encoding = "UTF-8")
 data$Klassenbreite <- cut(data$Alter, breaks = seq(0, 80, 5), right = FALSE)
 
-ggplot(data, aes(x = Klassenbreite, fill = factor(Person > 8))) +
-  geom_bar(position = "identity", alpha = 0.5) +
+ggplot(data, aes(x = factor(Person > 8), y = Alter, fill = factor(Person > 8))) +
+  geom_boxplot() +
   scale_fill_manual(values = c("#619CFF", "#FF7A5A"), name = "Datensatz",
                     labels = c("Vorstudie", "Masterarbeit")) +
-  labs(x = "Alter", y = "Personen")
+  scale_x_discrete(labels=c("Vorstudie", "Masterarbeit")) +
+  labs(x = "Datensatz", y = "Alter") +
+  theme_bw()
+
+study <- subset(data, Person <= 8)
+summary(study$Alter)
+
+thesis <- subset(data, Person > 8)
+summary(thesis$Alter)
