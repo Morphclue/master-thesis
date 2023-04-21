f50baccedc3f6dcb44e04f1d7201cf6e91ade8e7
Julian Holfeld <julian@uni-kassel.de>
2023-04-21 12:13:18 +0200

    refactor: representation of hist

--- code/news-frequency.r
+++ code/news-frequency.r
@@ -2,14 +2,9 @@ if (!require('pacman')) install.packages('pacman')
 p_load(pacman)
 
 frequency <- c(0, 14, 1, 0.25, 14, 14, 2, 1, 3, 7)
+classes <- c(0, 6, 14)
 
-hist(frequency, breaks = seq(0, 15, 3), col = '#619CFF', border = 'black',
+hist(frequency, breaks = classes, col = '#619CFF', border = 'black',
      main = "",
      xlab = "News-Konsum pro Woche",
-     ylab = "Anzahl der Personen",
-     axes = FALSE)
-
-class_widths <- diff(seq(0, 15, 3))
-class_labels <- paste0("(", seq(0, 12, 3), ", ", seq(3, 15, 3), "]")
-axis(side = 1, at = seq(1.5, 13.5, by = 3), labels = class_labels)
-axis(side = 2)
+     ylab = "relative Anzahl der Personen")
